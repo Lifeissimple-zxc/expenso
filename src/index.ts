4970049371bcccbbacf692c24070f4f3a269a10d
Lifeissimple-zxc <mararkarp@gmail.com>
2026-02-20 23:25:27 +0100

    claude fixed tsconfig issue for me

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,14 @@
 import dotenv from "dotenv"
 import { Telegraf } from "telegraf"
-import { env } from "~/config/env.ts"
+import { env } from "#~/config/env"
 
 dotenv.config()
-
-console.log(process.env)
 
 const bot = new Telegraf(env.TELEGRAM_BOT_TOKEN)
 
 bot.start((ctx) => {
 	ctx.reply("Cheers, send me a pic of your expense and i will try to parse it")
 })
-
-bot
-	.launch()
-	.then(() => console.log("Bot is polling..."))
-	.catch((err) => console.error("Launch failed:", err))
 
 bot
 	.launch()
